b9ccd32ebebb395dc0cf2c0dd7920960b4fe092d
volhfx07132 <volhfx07132@funix.edu.vn>
2022-03-22 11:41:38 +0700

    libraries uniswapV2

---
+++ libraries/SafeERC20.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+import "../interfaces/IERC20.sol";
+
+library SafeERC20 {
+    // Get symbol of token after action encode of address token
+    function safeSymbol(IERC20 token) internal view returns(string memory) {
+        (bool success, bytes memory data) = address(token).staticcall(abi.encodeWithSelector(0x95d89b41));
+        return success && data.length > 0 ? abi.decode(data, (string)) : "???";
+    }
+    // Get name of token after action encode of address token
+    function safeName(IERC20 token) internal view returns(string memory) {
+        (bool success, bytes memory data) = address(token).staticcall(abi.encodeWithSelector(0x06fdde03));
+        return success && data.length > 0 ? abi.decode(data, (string)) : "???";
+    }
+    // get decimel of token after action encode of address token
+    function safeDecimals(IERC20 token) public view returns (uint8) {
+        (bool success, bytes memory data) = address(token).staticcall(abi.encodeWithSelector(0x313ce567));
+        return success && data.length == 32 ? abi.decode(data, (uint8)) : 18;
+    }
+    // transfer token after action encode of token address
+    function safeTransfer(IERC20 token, address to, uint256 amount) internal {
+        (bool success, bytes memory data) = address(token).call(abi.encodeWithSelector(0xa9059cbb, to, amount));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), "SafeERC20: Transfer failed");
+    }
+    // transferFrom token after encode of token address
+    function safeTransferFrom(IERC20 token, address from, uint256 amount) internal {
+        (bool success, bytes memory data) = address(token).call(abi.encodeWithSelector(0x23b872dd, from, address(this), amount));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), "SafeERC20: TransferFrom failed");
+    }
+}
